7eb71f0ffbae43679b879e25e6f07c4018d69e40
kadeem-lewis <email@kadeemlewis.com>
2023-11-08 17:21:56 -0500

    updated presentation to work with react 18

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import AddReview from "./components/addReview";
+import MoviesList from "./components/moviesList";
+import Movie from "./components/movie";
+import Login from "./components/login";
+
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 
 function App() {
+  const [user, setUser] = useState(null);
+
+  async function login(user = null) {
+    setUser(user);
+  }
+
+  async function logout() {
+    setUser(null);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand>Movie Reviews</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link>
+              <Link to={"/movies"}>Movies</Link>
+            </Nav.Link>
+            <Nav.Link>
+              {user ? <a>Logout User</a> : <Link to={"/login"}>Login</Link>}
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+      <Routes>
+        <Route exact path="/" element={MoviesList}></Route>
+        <Route path="/movies" element={MoviesList}></Route>
+        <Route
+          path="/movies/:id/review"
+          element={<AddReview user={user} />}
+        ></Route>
+        <Route path="/movies/:id/" element={<Movie user={user} />}></Route>
+        <Route path="/login" element={<Login login={login} />}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/components/moviesList.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function MoviesList() {
+  return <div className="App">Movies List</div>;
+}
