9e3fc6efa60f721d778bb66bb05f6eb7bdc6f8d7
kadeem-lewis <email@kadeemlewis.com>
2023-11-15 00:18:43 -0500

    updated make of axios services file to better match powerpoint

--- src/components/moviesList.js
+++ src/components/moviesList.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import MovieDataService from "../services/movies";
+import MovieDataService from "../services/moviesDataService";
 import { Link } from "react-router-dom";
 
 import Form from "react-bootstrap/Form";
